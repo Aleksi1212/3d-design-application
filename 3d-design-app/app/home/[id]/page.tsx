e6bb7390ca24b6139c8590b97f2ed942e81db9e7
Aleksi1212 <aleksinoro@gmail.com>
2023-01-16 20:27:28 +0200

    user home page changes

--- 3d-design-app/app/home/[id]/page.tsx
+++ 3d-design-app/app/home/[id]/page.tsx
@@ -1,14 +1,78 @@
 import UserHome from "../../../src/components/userHome";
 import getUserData from "../../../src/datalayer/getUserData";
 
+import Image from "next/image";
+
+import addDoc from '../../../src/images/addDoc.png'
+import docMenu from '../../../src/images/docMenu.png'
+import docShare from '../../../src/images/docShare.png'
+import docRemove from '../../../src/images/docRemove.png'
+
 async function UserHomePage({ params }: any) {
-    const test = await getUserData({ userId: params.id })
-    test?.map((card) => {
-        console.log(card.email);
-    })
+    const userData = await getUserData({ userId: params.id })
+    let documents: any = []
+
+    userData.map((card) => {
+        card.documents.forEach((doc: any) => {
+            documents.push(doc)
+        })
+    })    
+
+    // function generateRandom() {
+    //     let text = ''
+    //     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    //     for (let i = 0; i < 8; i++) {
+    //         text += chars.charAt(Math.floor(Math.random() * chars.length))
+    //     }
+
+    //     return text
+    // }
+
+    // const testt = generateRandom()
+    // console.log(testt);
+    
 
     return (
-        <UserHome />
+        <>
+            <div className="absolute top-0 w-full h-[100vh] flex justify-center">
+                <div className="max-w-[66rem] top-[15rem] absolute flex gap-y-12 gap-x-12 flex-wrap ">
+
+                    <div className="bg-white rounded-lg shadow-xl h-[15rem] w-[20rem] flex justify-center items-center cursor-pointer" id="doc">
+                        <div className="flex flex-col items-center text-[#1A73E8] gap-y-8 mt-8">
+                            <Image src={addDoc} alt="addDoc" />
+                            <h1>Add New Document</h1>
+                        </div>
+                    </div>
+
+                    {        
+                        documents.map((docCard: any) => {
+                            return <DocumentCard key={docCard.docId} document={docCard} />
+                        })
+                    }
+                </div>
+            </div>
+            <UserHome />
+        </>
+    )
+}
+
+function DocumentCard({ document }: any) {
+    const { docName } = document || {}
+
+    return (
+        <div className="bg-white rounded-lg shadow-xl h-[15rem] w-[20rem] flex justify-between flex-col cursor-pointer" id="doc">
+            <h1 className="pt-3 pl-4 text-xl">{docName}</h1>
+
+            <div className="flex justify-between pb-3">
+                <Image src={docMenu} alt="docMenu" className="ml-4" />
+                
+                <div className="flex justify-evenly w-[6rem]">
+                    <Image src={docShare} alt="docShare" />
+                    <Image src={docRemove} alt="docRemove" />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- 3d-design-app/src/components/userHome.tsx
+++ 3d-design-app/src/components/userHome.tsx
@@ -3,14 +3,30 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
-import arrow1 from '../images/arrow1.png'
-// import arrow1_2 from '../images/arrow1-2.svg'
+import arrow1 from '../images/arrow1.svg'
+import arrow2 from '../images/arrow2.svg'
+import arrow3 from '../images/arrow3.svg'
+import cross from '../images/cross.svg'
 
 function UserHome() {
     return (
-        <section className="bg-[#F6F7F9] absolute w-full h-[100vh]">
-            <div className="bg-[#1A73E8] h-[40%]">
-                {/* <Image src={arrow1} alt="arrow1" /> */}
+        <section className="bg-[#F6F7F9] w-full h-[150vh]">
+            <div className="bg-[#1A73E8] h-[30%] flex">
+                <div className="flex flex-col justify-end pb-20 pl-24">
+                    <Image src={arrow1} alt="arrow1" />
+                </div>
+                
+                <div className="flex flex-col justify-start pt-16 pl-72">
+                    <Image src={cross} alt="cross" />
+                </div>
+
+                <div className="flex flex-col justify-center pl-[30rem]">
+                    <Image src={arrow2} alt="arrow2" />
+                </div>
+
+                <div className="flex flex-col justify-end pl-96 pb-10">
+                    <Image src={arrow3} alt="arrow3" />
+                </div>
             </div>
         </section>
     ) 
