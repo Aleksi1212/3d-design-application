aead7b0ca4d06491fe25a77c5b56ed15466bebe9
Aleksi1212 <aleksinoro@gmail.com>
2023-01-25 18:37:01 +0200

    realtime changes

--- 3d-design-app/app/dashboard/[id]/page.tsx
+++ 3d-design-app/app/dashboard/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import UserHome from "../../../src/components/userHome";
 import { getUserData } from "../../../src/datalayer/querys";
 import Profile from "../../../src/components/profileCard";
@@ -10,33 +12,64 @@ import docMenu from '../../../src/images/docMenu.png'
 import docShare from '../../../src/images/docShare.png'
 import docRemove from '../../../src/images/docRemove.png'
 
-async function UserHomePage({ params }: any) {
-    const userData = await getUserData({ userId: params.id })
+import { useEffect, useState } from "react";
 
-    let documents: any = []
-    let userName: string = ''
-    let userEmail: string = ''
-    let userMethod: string = ''
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../../../src/datalayer/config";
 
-    userData.map((card) => {
-        card.documents.forEach((doc: any) => {
-            documents.push(doc)
+import { updateDesign } from "../../../src/datalayer/querys";
+
+function UserHomePage({ params }: any) {
+    // const userData = await getUserData({ userId: params.id })
+
+    // let documents: any = []
+    // let userName: string = ''
+    // let userEmail: string = ''
+    // let userMethod: string = ''
+
+    // userData.map((card) => {
+    //     card.documents.forEach((doc: any) => {
+    //         documents.push(doc)
+    //     })
+
+    //     userName = card.username
+    //     userEmail = card.email
+    //     userMethod = card.method
+    // })
+
+    const [userData, setUserData] = useState([])
+
+    useEffect(() => {
+        const que = query(collection(db, 'data'), where('userId', '==', params.id))
+
+        const getUserData = onSnapshot(que, (querySnapshot) => {
+            let data: any = []
+            querySnapshot.forEach((doc) => {
+                data.push(doc.data())
+            })
+
+            setUserData(data)
         })
 
-        userName = card.username
-        userEmail = card.email
-        userMethod = card.method
+        return () => getUserData()
+    }, [])
+
+    let docs: any = []
+
+    userData.map((data) => {
+        data.documents.forEach((doc) => {
+            docs.push(doc)
+        })
     })
-
-
 
     return (
         <>
             <div className="absolute top-0 w-full h-[150vh] flex items-center flex-col">
-                <h1 className="text-white text-5xl mt-24">Welcome Back {userName}</h1>
+                <h1 className="text-white text-5xl mt-24">Welcome Back {'test'}</h1>
 
                 <div className="max-w-[66rem] my-[6rem] flex gap-y-12 gap-x-12 flex-wrap ">
-                        <div className="bg-white rounded-lg shadow-xl h-[15rem] w-[20rem] flex justify-center items-center cursor-pointer" id="doc">
+                        <div className="bg-white rounded-lg shadow-xl h-[15rem] w-[20rem] flex justify-center items-center cursor-pointer" id="doc"
+                            onClick={() => updateDesign(params.id, 'add', null, null)}>
                             <div className="flex flex-col items-center text-[#1A73E8] gap-y-8 mt-8">
                                 <Image src={addDoc} alt="addDoc" />
                                 <h1>Add New Design</h1>
@@ -44,37 +77,37 @@ async function UserHomePage({ params }: any) {
                         </div>
 
                     {
-                        documents.map((docCard: any) => {
-                            return <DocumentCard key={docCard.docId} document={docCard} />
+                        docs.map((docCard: any) => {
+                            return <DocumentCard key={docCard.docId} userId={params.id} docId={docCard.docId} docName={docCard.docName} />
                         })
                     }
                 </div>
 
                 <hr className="bg-[#5D5D5D] opacity-40 w-[50rem] pb-[1.5px]" />
 
-                <Profile userName={userName} userEmail={userEmail} id={params.id} method={userMethod} />
+                <Profile userName={'test'} userEmail={'test'} id={params.id} method={'test'} />
             </div>
             <UserHome />
         </>
     )
 }
 
-function DocumentCard({ document }: any) {
-    const { docName, docId } = document || {}
+function DocumentCard(props: any) {
+    console.log(props.userId);
 
     return (
-        <Link className="bg-white rounded-lg shadow-xl h-[15rem] w-[20rem] flex justify-between flex-col cursor-pointer" id="doc" href={`document/${docId}`}>
-            <h1 className="pt-3 pl-4 text-xl">{docName}</h1>
+        <div className="bg-white rounded-lg shadow-xl h-[15rem] w-[20rem] flex justify-between flex-col cursor-pointer" id="doc">
+            <h1 className="pt-3 pl-4 text-xl">{props.docName}</h1>
 
             <div className="flex justify-between pb-3">
                 <Image src={docMenu} alt="docMenu" className="ml-4" />
                 
                 <div className="flex justify-evenly w-[6rem]">
                     <Image src={docShare} alt="docShare" />
-                    <Image src={docRemove} alt="docRemove" />
+                    <Image src={docRemove} alt="docRemove" onClick={() => updateDesign(props.userId, 'remove', props.docId, props.docName)} />
                 </div>
             </div>
-        </Link>
+        </div>
     )
 }
 
--- 3d-design-app/src/datalayer/querys.ts
+++ 3d-design-app/src/datalayer/querys.ts
@@ -1,5 +1,5 @@
 import { db } from "./config"
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore"
+import { addDoc, collection, getDocs, query, where, doc, updateDoc, arrayUnion, arrayRemove } from "firebase/firestore"
 
 async function getUserData(data: any) {
     const que = query(collection(db, 'data'), where('userId', '==', data.userId))
@@ -46,8 +46,35 @@ async function cookieSetter(userState: boolean, userId: string | null) {
     return message.message as any
 }
 
+async function updateDesign(userId: string, type: string, desId: string | null, desName: string | null) {
+    let designId = ''
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (let i = 0; i < 8; i++) {
+        designId += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+
+    const que = query(collection(db, 'data'), where('userId', '==', userId))
+    const querySnapshot = await getDocs(que)
+    const docId = querySnapshot.docs.map((doc) => doc.id)
+
+    const docRef = doc(db, 'data', docId[0])
+
+    if (type === 'add') {
+        await updateDoc(docRef, {
+            documents: arrayUnion({ docId: designId, docName: 'Untitled' })
+        })
+
+    } else if (type === 'remove') {
+        await updateDoc(docRef, {
+            documents: arrayRemove({ docId: desId, docName: desName })
+        })
+    }
+}
+
 export {
     getUserData,
     checkUser,
-    cookieSetter
+    cookieSetter,
+    updateDesign,
 }
